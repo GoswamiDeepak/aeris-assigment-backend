de30125d21b12e306787983c0adeacdf8f7b163d
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-03-30 19:14:44 +0530

    vercel file update

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Assigment backend.!');
+});
 
 app.use('/api/v1/', vehicalrouter);
 
