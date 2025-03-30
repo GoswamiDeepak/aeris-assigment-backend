fbecf38c184712a8ed1d24835155f7005b8c2e46
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-03-30 19:39:18 +0530

    file modified

--- src/server.js
+++ src/server.js
@@ -2,12 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 8000;
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log('Listening on server: ', PORT);
-    });
-}
-
-// Export the Express app for Vercel
-export default app;
+app.listen(PORT, () => {
+    console.log('Listening on server: ', PORT);
+});
